7f0deb797aee974cd4a0ecbc01255b5a7f3b0178
Yuri Poeluev <yuri.poeluev@arcticwolf.com>
2018-05-04 14:43:13 -0400

    Make openvpn to always load OpenSSL config file, to support transparent TPM key load/use.

--- src/openvpn/ssl_openssl.c
+++ src/openvpn/ssl_openssl.c
@@ -75,6 +75,7 @@ tls_init_lib(void)
 #ifndef ENABLE_SMALL
     SSL_load_error_strings();
 #endif
+    OPENSSL_config(NULL);
     OpenSSL_add_all_algorithms();
 
     mydata_index = SSL_get_ex_new_index(0, "struct session *", NULL, NULL, NULL);
